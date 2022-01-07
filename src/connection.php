28976fddb8e93d0da08f1df7510a1aab91ce5517
Flavien Groslevin <fgroslevin@gmail.com>
2022-01-07 18:56:49 +0100

    add database heroku

--- src/connection.php
+++ src/connection.php
@@ -1,4 +1,4 @@
 <?php
-    $db = new PDO('mysql:host=localhost; dbname=netflix; charset=utf8', 'root', 'root');
+    $db = new PDO('mysql:host=ec2-52-209-111-18.eu-west-1.compute.amazonaws.com; dbname=netflix; charset=utf8', 'hfjqutourizfhz', '1350423b5f9a5355089889a9c2aff24e2132e77e4f1021bb121e4216c569827c');
 
 ?>
